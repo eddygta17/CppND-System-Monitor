b4273ffc7fd45f4746d3db09cce4cd0ad965c0d8
Abel <eddy.gta17@gmail.com>
2020-05-14 07:57:53 +0530

    format

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,12 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    char timestring[10];
+    int nanoH = seconds / 3600;
+    int remtime = seconds % 3600;
+    int nanoM = remtime / 60;
+    int nanoS = remtime % 60;
+    sprintf(timestring, "%.2d:%.2d:%.2d", nanoH, nanoM, nanoS);
+    return timestring;
+  }
