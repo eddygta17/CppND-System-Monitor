833426a36dd9c2db3a80158847373c6ff549eae0
Abel <eddy.gta17@gmail.com>
2020-05-14 07:50:00 +0530

    changed processor

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,23 @@
 #include "processor.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+  std::string cpu, user, nice, system, idle, iowait, irq, softirq, steal, guest,
+      guest_nice;
+  std::string line;
+  std::ifstream stream(LinuxParser::kProcDirectory +
+                       LinuxParser::kStatFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> cpu >> user >> nice >> system >> idle >> iowait >> irq >>
+        softirq >> steal >> guest >> guest_nice;
+  }
+
+  // Method from https://stackoverflow.com/a/23376195
+  int idletime = std::stoi(idle) + std::stoi(iowait);
+  int nonidletime = std::stoi(user) + std::stoi(nice) + std::stoi(system) + std::stoi(irq) + std::stoi(softirq) + std::stoi(steal);
+  float totaltime = idletime + nonidletime;
+  float util_ = (totaltime - idletime)/totaltime;
+  return util_;
+}
