72bc8a84d9b78e5c57044bb6c011ad659574d045
Abel <eddy.gta17@gmail.com>
2020-05-14 07:44:25 +0530

    Added process

--- src/process.cpp
+++ src/process.cpp
@@ -10,24 +10,41 @@ using std::string;
 using std::to_string;
 using std::vector;
 
+Process::Process(int pid)
+    : pid_(pid),
+      command_(LinuxParser::Command(pid)),
+      user_(LinuxParser::User(pid)){};
+
+
 // TODO: Return this process's ID
-int Process::Pid() { return 0; }
+int Process::Pid() { return pid_; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+float Process::CpuUtilization() {
+
+  float totaltime = LinuxParser::ActiveJiffies(Pid());  
+  float uptime = LinuxParser::UpTime();                 
+  float secondsactive =
+      uptime - (Process::UpTime() / sysconf(_SC_CLK_TCK));  
+  float cpu_usage =
+      (totaltime / sysconf(_SC_CLK_TCK)) / secondsactive;  
+  return cpu_usage;
+}
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+string Process::Command() { return command_; }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Ram() { return LinuxParser::Ram(Pid()); }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::User() { return user_; }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+long int Process::UpTime() { return return LinuxParser::UpTime(Pid()); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+bool Process::operator<(Process const& a) const {
+  return CpuUtilization() < a.CpuUtilization();
+}
